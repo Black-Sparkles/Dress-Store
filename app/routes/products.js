d89842ef0c869bb1d7c8438eefdd9895515d4fc3
Black-Sparkles <122908587+Black-Sparkles@users.noreply.github.com>
2023-10-14 22:30:26 -0400

    updated files

--- app/routes/products.js
+++ app/routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require("../controllers/products");
 
 router.get("/products", productController.getAllProducts);
-router.get("/products/:id", productController.productByID);
+router.get("/list/:id", productController.productByID);
 router.post("/create", productController.createProduct);
 router.put("/edit/:id", productController.update);
 router.delete("/delete/:id", productController.deleteProduct);
